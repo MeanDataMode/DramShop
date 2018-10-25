6e24da84ab9e2a5b92359f26105311ad455f3b0e
MeanDataMode <anthonylayton@gmail.com>
2018-10-25 12:58:19 -0600

    fine tune

--- Dram Consulting Primary.R
+++ Dram Consulting Primary.R
@@ -8,6 +8,7 @@
 #install.packages("prcomp")
 #install.packages("rpart")
 
+
 library(Matrix)
 library(MASS)
 library(foreign)
@@ -16,7 +17,8 @@ library(svd)
 library(prcomp)
 library(rpart)
 
-# Read in and structure the Production Data
+
+# Import and structure the data
 start.time <- Sys.time()
 data = read.csv(file = "data_Cust_Drink_Percent_Row.csv", header = T, sep = ",")
 rownames(data) <- data[,1]
@@ -25,24 +27,10 @@ data <- data[,which(colSums(data) != 0)] # Remove empty Columns
 data <- data[which(rowSums(data) != 0),] # removes empty Rows
 total.time <- Sys.time() - start.time
 total.time
-        # 26.74657 secs
-        # 12.17994 secs
-
-
-start.time <- Sys.time()
-data = read.csv(file = "data_Cust_Cat_Percent_Row.csv", header = T, sep = ",")
-rownames(data) <- data[,1]
-data <- data[,-1]
-data <- data[,which(colSums(data) != 0)] # Remove empty Columns
-data <- data[which(rowSums(data) != 0),] # removes empty Rows
-total.time <- Sys.time() - start.time
-total.time
-
-
 
 
 ######################################
-# More Stuff
+# Build SVD model and check variance
 start.time <- Sys.time()
 O = data / sum(data)
 E = rowSums(O) %o% colSums(O)
@@ -54,47 +42,48 @@ total.time <- Sys.time() - start.time
 total.time
         # 10.43311 mins
         # 10.03534 mins
-round(variance.explained,2)
-sum(round(variance.explained[1:1250],4))
-#
-##########
+number_of_factors = 735
+sum(variance.explained[1:number_of_factors])  # Looking for >= 70% explained
 
 
 # Reducing the Matrix down to nv
+# the irlba is a faster way of doing SVD
 start.time <- Sys.time()
-svd_irlba <- irlba(t(data), nv = 700,maxit = 1400)# *(TIP:nu = nv)
+svd_irlba <- irlba(t(data), nv = number_of_factors,maxit = (number_of_factors*2))  # (TIP:nu = nv)
 reduced_matrix <- (svd_irlba$u %*% diag(svd_irlba$d) %*% t(svd_irlba$v))
 total.time <- Sys.time() - start.time
 total.time
-        # 6.91502 secs
-        # 14.7105 secs
 
 
 # Shows me how far off we were 
-# (-1 = THe more the negative the less we need to recomend.
+# (-1 = THe more the negative the less we need to recomend. (our model underestimated the customers purchases)
 # 1 = THe higher the positive the stronger the recomendation to Try.
 recomendations <- reduced_matrix - data
 
 
-# Tease out one persons ('John Doe' aka 'Riley Zachariasen') recomendations
-john_actual <- data["Riley_Zachariasen",]
-john_predicted <- recomendations["Riley_Zachariasen",]
-john_recomendations <- john_predicted - john_actual
+# Look at an actual customers recomendations ('John Doe' aka 'Riley Zachariasen')
+johns_actual <- data["Riley_Zachariasen",]
+johns_predicted <- recomendations["Riley_Zachariasen",]
+johns_recomendations <- johns_predicted - johns_actual
 
-
-top_recomendations <- sort(john_recomendations, decreasing = TRUE)
+# Top Five to recommend to customer
+top_recomendations <- sort(johns_recomendations, decreasing = TRUE)
 how_many_recomendations = 5
 for (n in 1:how_many_recomendations) {
         print(top_recomendations[n])
 }
 
-botched_recomendations <- sort(john_recomendations, decreasing = FALSE)
+
+# Top 5 drinks that the customer purchased more than our model anticipated
+botched_recomendations <- sort(johns_recomendations, decreasing = FALSE)
 how_many_recomendations = 5
 for (n in 1:how_many_recomendations) {
         print(botched_recomendations[n])
 }
 
+
+# Look at one drink in particular
 the_drink = "Abyss.Deschutes"
-john_actual[,the_drink]
-john_predicted[,the_drink]
-john_recomendations[,the_drink]
+johns_actual[,the_drink]
+johns_predicted[,the_drink]
+johns_recomendations[,the_drink]
